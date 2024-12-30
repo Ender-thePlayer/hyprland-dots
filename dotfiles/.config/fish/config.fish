7cf66c6647421c7e43f5efda89291a5ac007f93d
Ender-thePlayer <enderjr2020@gmail.com>
2024-12-30 22:38:27 +0200

    updated things

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -1,3 +1,25 @@
+#set -Ux ELECTRON_OZONE_PLATFORM_HINT wayland
+
+pfetch
+
+# Initialize Starship for Fish
+starship init fish | source
+
+# Add a newline flag before each command execution
+function fish_user_key_bindings
+    # Ensure no newline is added before the prompt starts
+    set -g NEW_LINE_AFTER_PROMPT 0
+end
+
+# Add the newline flag after command execution
+function fish_postexec --on-event fish_postexec
+    # Set the flag to add a newline before the next prompt
+    set -g NEW_LINE_AFTER_PROMPT 1
+end
+
+set -x user $( ps -o user= -p $fish_pid | awk '{print $1}' )
+echo $user is thinking.. '"Kasane Teto on top"'
+
 if status is-interactive
     set -g fish_greeting
     set -x PATH $HOME/.local/bin $PATH
@@ -5,34 +27,31 @@ if status is-interactive
     set -x MOZ_ENABLE_WAYLAND 1
     set -x WLR_NO_HARDWARE_CURSORS 1
     set -x WLR_RENDERER_ALLOW_SOFTWARE 1
-    set -x QT_QPA_PLATFORM xcb
+#    set -x QT_QPA_PLATFORM xcb
 
-    function sb
-    	sudo sbctl sign -s /boot/EFI/BOOT/BOOTX64.EFI
-    	sudo sbctl sign -s /boot/EFI/systemd/systemd-bootx64.efi
-    	sudo sbctl sign -s /boot/vmlinuz-linux
-    	sudo sbctl sign -s /boot/vmlinuz-linux-lts
-    	sudo sbctl sign -s /boot/vmlinuz-linux-zen
-    end
+    set -x PATH $HOME/.venv/bin $PATH
 
-    function frepair
-        sudo systemctl restart python3-validity.service
-        sudo systemctl restart python3-validity-wakeup.service
-    end
+    set -x date "$( math $( date +%Y ) +1 )"
 
-    function spicetifyfix
-        chmod a+wr $HOME/.local/share/flatpak/app/com.spotify.Client/x86_64/stable/active/files/extra/share/spotify
-        chmod a+wr -R $HOME/.local/share/flatpak/app/com.spotify.Client/x86_64/stable/active/files/extra/share/spotify/Apps
-        spicetify update
-        $HOME/.spicetify/spicetify backup
-    end
-
-    alias 2024 'echo "YEAR OF THE DESKTOP"'
+    alias $date 'echo "YEAR OF THE DESKTOP"'
     alias 17 'echo "セブンティーナァァァァァ!"'
     alias vencord 'sh -c "(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh | sh)"'
     alias freq 'watch -n.1 "cat /proc/cpuinfo | grep \"^[c]pu MHz\""'
     alias update_grub 'sudo grub2-mkconfig -o /boot/grub2/grub.cfg'
     alias dnf 'dnf5'
     alias sudo 'sudo '
-
 end
+
+
+#function starship_transient_rprompt_func
+# starship module time
+#end
+#starship init fish | source
+#enable_transience
+
+# pnpm
+set -gx PNPM_HOME "/home/ender/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
--- install.sh
+++ install.sh
@@ -1,44 +1,164 @@
 #!/usr/bin/env bash
 
-clear; echo "[INFO] Installing dependencies"
+while true; do
+    read -rp "Do you want to install paru AUR manager? [y/N]:" response
+    case "${response,,}" in 
+        y|yes)
+            echo "[INFO] Installing paru."
+            sudo pacman -S --noconfirm git flatpak
+            sudo pacman -S --needed --noconfirm base-devel
+            git clone https://aur.archlinux.org/paru-bin.git
+            cd paru-bin || exit 1
+            makepkg -si --noconfirm
+            cd ..
+            rm -rf ./paru-bin
+            sleep 3 && clear
+            break
+            ;;
+        n|no)
+            echo "[INFO] Skipping..."
+            sleep 3 && clear
+            break
+            ;;
+        *)
+            echo "[ERR] Invalid response."
+            ;;
+    esac
+done
 
-sudo pacman -Syu --noconfirm
-sudo pacman -S --noconfirm git flatpak
-sudo pacman -S --needed --noconfirm base-devel
+while true; do
+    read -rp "Do you want to install flatpak and configure flathub? [y/N]:" response
+    case "${response,,}" in 
+        y|yes)
+            echo "[INFO] Installing flatpak."
+            sudo pacman -S --noconfirm flatpak
+            flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+            sleep 3 && clear
+            break
+            ;;
+        n|no)
+            echo "[INFO] Skipping..."
+            sleep 3 && clear
+            break
+            ;;
+        *)
+            echo "[ERR] Invalid response."
+            ;;
+    esac
+done
 
-git clone https://aur.archlinux.org/paru-bin.git
-cd paru-bin || exit 1
-makepkg -si --noconfirm
-cd ..
-rm -rf ./paru-bin
-
-sudo pacman -S --noconfirm kitty waybar fish hypridle hyprlock hyprpaper rofi-wayland gammastep brightnessctl pavucontrol pamixer blueberry gnome-tweaks papirus-icon-theme dunst network-manager-applet wl-clipboard fcitx5-config-qt kdeconnect polkit-gnome cliphist noto-fonts-cjk xdg-desktop-portal-gtk xdg-desktop-portal nautilus vfs-mtp neovim ttf-jetbrains-mono-nerd swaync
-paru -S --noconfirm qt5ct-kde qt6ct-kde hyprpicker hyprshot wlogout fcitx5-mozc vesktop-bin librewolf-bin
-sudo udevadm control --reload
-
-flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install --user -y flathub dev.vencord.Vesktop
-flatpak install --user -y flathub com.spotify.Client
-flatpak install --user -y flathub org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark
 
 
+echo "[INFO] Installing necessary apps."
 
-clear; echo "[INFO] Copying the dotfiles"
+pacman=(
+"xdg-user-dirs"
+"waybar"
+"hypridle"
+"hyprlock"
+"swww"
+"rofi-wayland"
+"wl-clipboard"
+"gammastep"
+"brightnessctl"
+"pavucontrol"
+"pamixer"
+"blueberry"
+"dunst"
+"network-manager-applet"
+"noto-fonts-cjk"
+"ttf-jetbrains-mono-nerd"
+"papirus-icon-theme"
+"xdg-desktop-portal-gtk"
+"xdg-desktop-portal"
+"plasma-framework5"
+"breeze5"
+"breeze-gtk"
+"fish"
+"starship"
+"cliphist"
+"mpv"
+"batsignal"
+"hyprpicker"
+"otf-latinmodern-math"
+)
 
-mkdir ./tmp
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > ./tmp/install.sh
-fish -c "./tmp/install.sh --path=~/.local/share/omf --config=~/.config/omf & wait -n fish && killall fish"
-rm -r ./tmp
+paru=(
+"qt5ct-kde"
+"qt6ct-kde"
+"hyprshot"
+"wlogout"
+"wttrbar"
+"nautilus-open-any-terminal"
+"pfetch-rs"
+"wl-screenrec"
+)
 
-gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+for pkg in "${pacman[@]}"; do
+    sudo pacman -S --noconfirm "$pkg"
+done
+
+for pkg in "${paru[@]}"; do
+    paru -S --noconfirm "$pkg"
+done
+
+flatpak=(
+"dev.vencord.Vesktop"
+"org.gtk.Gtk3theme.adw-gtk3"
+"org.gtk.Gtk3theme.adw-gtk3-dark"
+)
+
+for pkg in "${flatpak[@]}"; do
+    flatpak install --user -y flathub "$pkg"
+done
+
+sleep 3 && clear
+
+
+
+echo "[INFO] Copying the dotfiles"
 wget "$( curl -s https://api.github.com/repos/lassekongo83/adw-gtk3/releases/latest | grep browser_download_url | cut -d '"' -f 4 )"
-mkdir -p ~/.local/share/themes/
+mkdir ~/.local/share/themes/
 tar -xf ./adw-gtk3* -C ~/.local/share/themes/
 rm ./adw-gtk3*
 gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark' && gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface font-name "Inter 10"
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark' 
+
+sudo git clone https://github.com/keyitdev/sddm-astronaut-theme.git /usr/share/sddm/themes/sddm-astronaut-theme
+sudo cp /usr/share/sddm/themes/sddm-astronaut-theme/Fonts/* /usr/share/fonts/
+echo -e "[Theme]\nCurrent=sddm-astronaut-theme" | sudo tee /etc/sddm.conf > /dev/null
+
+git clone https://github.com/supermariofps/hatsune-miku-windows-linux-cursors
+mkdir -p ~/.local/share/icons/
+mv ./hatsune-miku-windows-linux-cursors/miku-cursor-linux ~/.local/share/icons
+rm -rf hatsune-miku-windows-linux-cursors
 
 glib-compile-schemas ~/.local/share/glib-2.0/schemas/
 gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal kitty
 
 shopt -s dotglob
 cp -r ./dotfiles/* "$HOME"/
+
+
+
+while true; do
+    read -rp "Do you want to change the default shell to fish? [y/N]:" response
+    case "${response,,}" in 
+        y|yes)
+            chsh -s "$(which fish)"
+            sleep 3 && clear
+            break
+            ;;
+        n|no)
+            echo "[INFO] Skipping..."
+            sleep 3 && clear
+            break
+            ;;
+        *)
+            echo "[ERR] Invalid response."
+            ;;
+    esac
+done
+
+echo "[DONE] Done installing the dotfiles!"
